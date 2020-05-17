6a06564f37ac6ec81423bd7a856290a5791d7f64
Byurhan Beyzat <imbyurhan@gmail.com>
2020-05-17 14:35:07 +0300

    Update apolo cient

--- src/lib/withApollo.js
+++ src/lib/withApollo.js
@@ -2,26 +2,45 @@ import withApollo from 'next-with-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
 import fetch from 'isomorphic-unfetch'
+
+if (!process.browser) {
+  global.fetch = fetch
+}
 
 export default withApollo(({ initialState, headers }) => {
   const isBrowser = typeof window !== 'undefined'
+  const backendUrl =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:2000'
+      : 'https://bank-api.bbn.codes'
+
+  const httpLink = createHttpLink({
+    uri: `${backendUrl}/graphql`,
+    credentials: 'include',
+    headers: {
+      ...headers,
+      Cookie: headers?.cookie || '',
+    },
+  })
 
   return new ApolloClient({
+    credentials: 'include',
     connectToDevTools: isBrowser,
     ssrMode: !isBrowser,
-    link: createHttpLink({
-      uri:
-        process.env.NODE_ENV === 'development'
-          ? 'http://localhost:2000/graphql'
-          : 'https://bank-api.bbn.codes/graphql',
-      credentials: 'include',
-      ...(!isBrowser && { fetch }),
-      headers: {
-        ...headers,
-        cookie: headers?.cookie,
-      },
-    }),
+    link: httpLink,
     cache: new InMemoryCache().restore(initialState || {}),
+    request: (operation) => {
+      operation.setContext({
+        fetchOptions: {
+          credentials: 'include',
+        },
+        headers: {
+          ...headers,
+          Cookie: headers?.cookie || '',
+        },
+      })
+    },
   })
 })
